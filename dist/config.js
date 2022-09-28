def0fbc3be9be662a40ea17d9fdd2d051b7d4fcf
Abdulbaki Dursun <eggsydev@gmail.com>
2022-09-28 23:53:12 +0300

    chore: add dist and env to gitignore

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    PAGE_URL: "http://egitimf.alparslan.edu.tr/tr/news-all",
-    BOT_TOKEN: "5767553242:AAGSQi41GwWb1Mw7_htWmtKvDWxjN48OEr4",
-};
-exports.default = config;
