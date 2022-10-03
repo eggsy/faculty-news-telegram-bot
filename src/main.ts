3e4e4df9684ef3b42283df22fa1919c189916869
Abdulbaki Dursun <eggsydev@gmail.com>
2022-10-04 01:26:33 +0300

    refactor: dont create directory, get rid of hop

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,31 @@
-import cron from "node-cron";
 import { config as loadEnv } from "dotenv";
 import { join } from "path";
-import { APIAuthentication, Hop } from "@onehop/js";
+
+import consola from "consola";
+import cron from "node-cron";
 
 loadEnv({
   path: join(process.cwd(), ".env.local"),
 });
 
 // Functions
-import { createDirectoryAndFile } from "./functions/createDirectoryAndFile";
 import { fetchAnnouncements } from "./functions/fetchAnnouncements";
 import { compareFiles } from "./functions/compareFiles";
 import { fetchNews } from "./functions/fetchNews";
 
 const fetchContent = async () => {
-  console.info("[CRON] Fetching news...");
+  consola.info("[CRON] Fetching news...");
 
   const news = await fetchNews();
   const announcemenets = await fetchAnnouncements();
+
   await compareFiles([...news, ...announcemenets]);
 };
 
 cron.schedule("*/30 * * * *", fetchContent);
 
 const init = async () => {
-  createDirectoryAndFile();
   fetchContent();
 };
-
-const HopToken = process.env.HOP_TOKEN as APIAuthentication;
-export const hop = new Hop(HopToken);
 
 init();
