b0a1aa06a39873cb0be0f0c338a1cb1638b62b01
Abdulbaki Dursun <eggsydev@gmail.com>
2022-10-04 01:27:01 +0300

    chore: delete local file caching related functions

--- src/functions/updateLocalFile.ts
+++
@@ -1,7 +0,0 @@
-import { writeFileSync } from "fs";
-import type { NewsOrAnnouncements } from "../main";
-
-export const updateLocalFile = async (news: NewsOrAnnouncements[]) => {
-  const newsListString = JSON.stringify(news);
-  writeFileSync(`cache/news.json`, newsListString);
-};
