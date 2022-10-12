1bfe2cfd68287a5d5029db239b34e0bd9d679393
Abdulbaki Dursun <eggsydev@gmail.com>
2022-10-12 13:32:10 +0300

    feat: fetch events on cron job

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,29 @@ import consola from "consola";
 import cron from "node-cron";
 
 loadEnv({
-	path: join(process.cwd(), ".env.local"),
+  path: join(process.cwd(), ".env.local"),
 });
 
 // Functions
 import { fetchAnnouncements } from "./functions/fetchAnnouncements";
 import { compareFiles } from "./functions/compareFiles";
 import { fetchNews } from "./functions/fetchNews";
+import { fetchEvents } from "./functions/fetchEvents";
 
 const fetchContent = async () => {
-	consola.info("[CRON] Fetching news...");
+  consola.info("[CRON] Fetching news, announcements and events...");
 
-	const news = await fetchNews();
-	const announcemenets = await fetchAnnouncements();
+  const news = await fetchNews();
+  const announcemenets = await fetchAnnouncements();
+  const events = await fetchEvents();
 
-	await compareFiles([...news, ...announcemenets]);
+  await compareFiles([...news, ...announcemenets], events);
 };
 
 cron.schedule("*/30 * * * *", fetchContent);
 
 const init = async () => {
-	fetchContent();
+  fetchContent();
 };
 
 init();
