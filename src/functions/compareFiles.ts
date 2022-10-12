b99b34dd894ac3528435d4178777b51c52b45cb3
Abdulbaki Dursun <eggsydev@gmail.com>
2022-10-12 13:33:00 +0300

    feat: compare new events and send for approval

--- src/functions/compareFiles.ts
+++ src/functions/compareFiles.ts
@@ -2,13 +2,17 @@ import consola from "consola";
 
 // Types
 import type { NewsOrAnnouncements } from "../@types/news";
+import type { IEvent } from "src/@types/events";
 
 // Functions
 import { updateCloudRecords } from "./updateCloudRecords";
-import { sendForApproval } from "./sendForApproval";
+import { sendForApproval, sendImageForApproval } from "./sendForApproval";
 import { getNewsState } from "./getNewsState";
 
-export const compareFiles = async (news: NewsOrAnnouncements[]) => {
+export const compareFiles = async (
+  news: NewsOrAnnouncements[],
+  events: IEvent[]
+) => {
   const result = await getNewsState();
   if (!result.news) return;
 
@@ -19,13 +23,25 @@ export const compareFiles = async (news: NewsOrAnnouncements[]) => {
     );
   });
 
-  if (newNews.length <= 0) return;
+  const newEvents = events.filter((item) => {
+    return !result.events?.find(
+      (localItem: IEvent) => localItem.title === item.title
+    );
+  });
 
-  consola.info(
-    `[COMPARE] Found ${newNews.length} news, updating the DB and posting for approval.`
-  );
+  if (newNews.length <= 0 && newEvents.length <= 0) return;
 
-  await updateCloudRecords(news);
+  if (newNews.length)
+    consola.info(
+      `[COMPARE] Found ${newNews.length} news, updating the DB and posting for approval.`
+    );
+
+  if (newEvents.length)
+    consola.info(
+      `[COMPARE] Found ${newEvents.length} events, updating the DB and posting for approval.`
+    );
+
+  await updateCloudRecords(news, events);
 
   for (const item of newNews) {
     sendForApproval(
@@ -34,5 +50,9 @@ export const compareFiles = async (news: NewsOrAnnouncements[]) => {
       }\n\n${item.description}`,
       item.link
     );
+  }
+
+  for (const item of newEvents) {
+    sendImageForApproval(`✨ *${item.title}*`, item.image, item.link);
   }
 };
