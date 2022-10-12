eb46894fc0004d8b238c3b76d9028355eb65c19c
Abdulbaki Dursun <eggsydev@gmail.com>
2022-10-12 13:32:30 +0300

    chore: add event type to state return type

--- src/functions/getNewsState.ts
+++ src/functions/getNewsState.ts
@@ -1,8 +1,12 @@
-import { NewsOrAnnouncements } from "../@types/news";
 import { hop } from "../hop";
+
+// Types
+import type { IEvent } from "src/@types/events";
+import type { NewsOrAnnouncements } from "../@types/news";
 
 export const getNewsState = async () => {
   return (await hop.channels.get("maun-news")).state as {
     news: NewsOrAnnouncements[];
+    events: IEvent[];
   };
 };
