14b84f9e17d10821d8802825c97361998b072317
Abdulbaki Dursun <eggsydev@gmail.com>
2022-10-31 23:46:26 +0300

    feat: move bot file to source root

--- src/bot.ts
+++ src/bot.ts
@@ -3,15 +3,15 @@ import { readdirSync } from "fs";
 import { join } from "path";
 
 // Types
-import { CommandExecute, Command } from "../@types/command";
+import { CommandExecute, Command } from "./@types/command";
 
 // Handlers
-import { messageHandler } from "../handlers/message";
-import { callbackQueryHandler } from "../handlers/callbackQuery";
+import { messageHandler } from "./handlers/message";
+import { callbackQueryHandler } from "./handlers/callbackQuery";
 
 if (!process.env.BOT_TOKEN) throw new Error("BOT_TOKEN is not defined.");
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {
+export const bot = new TelegramBot(process.env.BOT_TOKEN, {
   polling: true,
 });
 
@@ -36,5 +36,3 @@ bot.setMyCommands(botCommands);
 
 bot.on("message", messageHandler);
 bot.on("callback_query", callbackQueryHandler);
-
-export default bot;
